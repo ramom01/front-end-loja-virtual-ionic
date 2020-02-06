44dd17b5b8166da131cfdd30c59f4ababa1e6411
Ramon Rodrigues <rrsalescc@hotmail.com>
2020-02-06 09:50:01 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,3 +1,4 @@
+import { FieldMessage } from './../models/fieldmessage';
 import { StorageService } from './../services/storage.service';
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
@@ -36,6 +37,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                         this.handle403()
                         break
 
+                    case 422:
+                        this.handle422(errorObj)
+                        break
+
                     default:
                         this.handleDefaultError(errorObj)
                 }
@@ -62,6 +67,20 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present()
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Error 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        })
+        alert.present()
+    }
+
     handleDefaultError(errorObj) {
         let alert = this.alertCtrl.create({
             title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
@@ -74,6 +93,14 @@ export class ErrorInterceptor implements HttpInterceptor {
             ]
         })
         alert.present()
+    }
+
+    private listErrors(messages: FieldMessage[]): string {
+        let s: string = ""
+        for (var i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + '</strong>: ' + messages[i].message + '</p>'
+        }
+        return s
     }
 }
 
