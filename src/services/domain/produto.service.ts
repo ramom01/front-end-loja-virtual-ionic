c8abba2c41f346d3892b0b14c59750ab33d26b60
Ramon Rodrigues <rrsalescc@hotmail.com>
2020-02-07 11:01:23 -0300

    Infinite scroll

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -14,17 +14,17 @@ export class ProdutoService {
         return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`)
     }
 
-    findByCategoria(categoria_id: string) {
-        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`)
-    }
-
-    getImageFromBucket(id: string): Observable<any> {
-        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
-        return this.http.get(url, { responseType: 'blob' })
+    findByCategoria(categoria_id: string, page: number = 0, linesPerPage: number = 24) {
+        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`)
     }
 
     getSmallImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, { responseType: 'blob' })
+    }
+
+    getImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
         return this.http.get(url, { responseType: 'blob' })
     }
 }
