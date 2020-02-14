52e701982e666ff9c7ec702615cda005c59ec66e
Ramon Rodrigues <rrsalescc@hotmail.com>
2020-02-14 17:59:42 -0300

    Obtendo imagem da galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -64,7 +64,24 @@ export class ProfilePage {
 
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data:image/png;base64,' + imageData
+      this.cameraOn = false
+    }, (err) => { })
+  }
+
+  getGalleryPicture() {
+    this.cameraOn = true
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
